d2eaf5c3fafea2a1eb11403dae8551ba5bac4d05
Charles-04 <charlesmir04@gmail.com>
2023-03-28 11:01:32 +0100

    Feat:Added Fund Controller

--- peer_to_peer_money_transfer/peer_to_peer_money_transfer.API/Controllers/FundController.cs
+++ peer_to_peer_money_transfer/peer_to_peer_money_transfer.API/Controllers/FundController.cs
@@ -1,11 +1,34 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+
 using Microsoft.AspNetCore.Mvc;
+using PayStack.Net;
+using peer_to_peer_money_transfer.BLL.Implementation;
+using peer_to_peer_money_transfer.BLL.Infrastructure;
+using peer_to_peer_money_transfer.BLL.Interfaces;
+using peer_to_peer_money_transfer.DAL.Dtos.Requests;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace peer_to_peer_money_transfer.API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("CashMingle/[controller]")]
     [ApiController]
     public class FundController : ControllerBase
     {
+        IFundingService _fundingService;
+        public FundController(IFundingService fundingService)
+        {
+            _fundingService = fundingService;
+        }
+        [AllowAnonymous]
+        [HttpPost("fund-account")]
+        [SwaggerOperation(Summary = "Funds account using paystack")]
+        [SwaggerResponse(StatusCodes.Status200OK, Description = "Funding successful", Type = typeof(SuccessResponse))]
+        [SwaggerResponse(StatusCodes.Status400BadRequest, Description = "Transaction failed", Type = typeof(ErrorResponse))]
+        [SwaggerResponse(StatusCodes.Status500InternalServerError, Description = "It's not you, it's us", Type = typeof(ErrorResponse))]
+        public async Task<ActionResult<TransactionInitializeResponse>> Deposit(DepositRequest depositRequest)
+        {
+            var response = _fundingService.MakePayment(depositRequest);
+            return response;
+        }
     }
 }
