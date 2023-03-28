f76b97fa791d686205383932ec7acfe186495fa7
Charles-04 <charlesmir04@gmail.com>
2023-03-28 11:09:16 +0100

    Feat : Added Deposit Request DTO

---
+++ peer_to_peer_money_transfer/peer_to_peer_money_transfer.DAL/Dtos/Requests/DepositRequest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace peer_to_peer_money_transfer.DAL.Dtos.Requests
+{
+    public class DepositRequest
+    {
+        [Required]
+        public string CurrentUserId { get; set; }
+        [Required]
+        public string Reference { get; set; }
+
+        [JsonProperty("amount")]
+        [Required]
+        public int AmountInKobo { get; set; }
+        [Required]
+        public string Email { get; set; }
+
+        public string Plan { get; set; }
+
+        [JsonProperty("callback_url")]
+        public string CallbackUrl { get; set; }
+
+        [JsonProperty("subaccount")]
+        public string SubAccount { get; set; }
+        [Required]
+        [JsonProperty("transaction_charge")]
+        public int TransactionCharge { get; set; }
+
+        [JsonProperty("currency")]
+        public string Currency { get; set; } = "NGN";
+
+        public string Bearer { get; set; }
+    }
+}
