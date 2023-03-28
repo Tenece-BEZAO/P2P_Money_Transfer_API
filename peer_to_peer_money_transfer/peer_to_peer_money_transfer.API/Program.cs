7534747aa760b2811ffd08b9afcf11468b6f30e6
Charles-04 <charlesmir04@gmail.com>
2023-03-28 11:03:32 +0100

    Chore: Added DI for services

--- peer_to_peer_money_transfer/peer_to_peer_money_transfer.API/Program.cs
+++ peer_to_peer_money_transfer/peer_to_peer_money_transfer.API/Program.cs
@@ -1,6 +1,11 @@
 using Microsoft.EntityFrameworkCore;
-using PayStack.Net;
+using Microsoft.Extensions.Configuration;
+using System.Reflection;
 using peer_to_peer_money_transfer.DAL.Context;
+using peer_to_peer_money_transfer.BLL.Interfaces;
+using peer_to_peer_money_transfer.BLL.Implementation;
+using peer_to_peer_money_transfer.DAL.Interfaces;
+using peer_to_peer_money_transfer.DAL.Implementation;
 
 namespace peer_to_peer_money_transfer.API
 {
@@ -16,7 +21,9 @@ namespace peer_to_peer_money_transfer.API
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-
+            builder.Services.AddScoped<IFundingService, FundingService>();
+            builder.Services.AddScoped<IUnitOfWork, UnitOfWork<ApplicationDBContext>>();
+            builder.Services.AddAutoMapper(Assembly.Load("peer_to_peer_money_transfer.DAL"));
             string connectionString = builder.Configuration.GetConnectionString("DefaultConn");
             builder.Services.AddDbContext<ApplicationDBContext>(options => options.UseSqlServer(
             builder.Configuration.GetConnectionString("DefaultConn")));
