5299a60835bb54ec68933036bdfaf87bf32a7a7c
Charles-04 <charlesmir04@gmail.com>
2023-04-06 15:25:42 +0100

    Ran Migrations

--- peer_to_peer_money_transfer/peer_to_peer_money_transfer.DAL/Migrations/20230406140157_UpdatedUserEntity.Designer.cs
+++ peer_to_peer_money_transfer/peer_to_peer_money_transfer.DAL/Migrations/20230406140157_UpdatedUserEntity.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using peer_to_peer_money_transfer.DAL.Context;
 
@@ -11,9 +12,11 @@ using peer_to_peer_money_transfer.DAL.Context;
 namespace peer_to_peer_money_transfer.DAL.Migrations
 {
     [DbContext(typeof(ApplicationDBContext))]
-    partial class ApplicationDBContextModelSnapshot : ModelSnapshot
+    [Migration("20230406140157_UpdatedUserEntity")]
+    partial class UpdatedUserEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
